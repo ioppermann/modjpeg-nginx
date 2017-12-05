dbd7ab3d9f20e683d53b8e30f94c7950d70af841
ioppermann <ingo.oppermann@gmail.com>
2017-12-05 20:44:02 +0100

    Rename functions and variables to jpeg_filter

--- ngx_http_jpeg_filter_module.c
+++ ngx_http_jpeg_filter_module.c
@@ -11,7 +11,7 @@ typedef struct {
 	ngx_flag_t	progressive;
 
 	size_t		buffer_size;
-} ngx_http_modjpeg_loc_conf_t;
+} ngx_http_jpeg_filter_loc_conf_t;
 
 typedef struct {
 	u_char		*image;
@@ -25,57 +25,57 @@ typedef struct {
 	ngx_uint_t	max_height;
 
 	ngx_uint_t	phase;
-} ngx_http_modjpeg_ctx_t;
+} ngx_http_jpeg_filter_ctx_t;
 
-static ngx_command_t ngx_http_modjpeg_commands[] = {
-	{ ngx_string("modjpeg"),
+static ngx_command_t ngx_http_jpeg_filter_commands[] = {
+	{ ngx_string("jpeg_filter"),
 	  NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
 	  ngx_conf_set_flag_slot,
 	  NGX_HTTP_LOC_CONF_OFFSET,
-	  offsetof(ngx_http_modjpeg_loc_conf_t, enabled),
+	  offsetof(ngx_http_jpeg_filter_loc_conf_t, enabled),
 	  NULL },
 
-	{ ngx_string("modjpeg_max_width"),
+	{ ngx_string("jpeg_filter_max_width"),
 	  NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
 	  ngx_conf_set_num_slot,
 	  NGX_HTTP_LOC_CONF_OFFSET,
 	  offsetof(ngx_http_modjpeg_loc_conf_t, max_width),
 	  NULL },
 
-	{ ngx_string("modjpeg_max_height"),
+	{ ngx_string("jpeg_filter_max_height"),
 	  NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
 	  ngx_conf_set_num_slot,
 	  NGX_HTTP_LOC_CONF_OFFSET,
-	  offsetof(ngx_http_modjpeg_loc_conf_t, max_height),
+	  offsetof(ngx_http_jpeg_filter_loc_conf_t, max_height),
 	  NULL },
 
-	{ ngx_string("modjpeg_optimize"),
+	{ ngx_string("jpeg_filter_optimize"),
 	  NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
 	  ngx_conf_set_flag_slot,
 	  NGX_HTTP_LOC_CONF_OFFSET,
-	  offsetof(ngx_http_modjpeg_loc_conf_t, optimize),
+	  offsetof(ngx_http_jpeg_filter_loc_conf_t, optimize),
 	  NULL },
 
-	{ ngx_string("modjpeg_progressive"),
+	{ ngx_string("jpeg_filter_progressive"),
 	  NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
 	  ngx_conf_set_flag_slot,
 	  NGX_HTTP_LOC_CONF_OFFSET,
-	  offsetof(ngx_http_modjpeg_loc_conf_t, progressive),
+	  offsetof(ngx_http_jpeg_filter_loc_conf_t, progressive),
 	  NULL },
 
-	{ ngx_string("image_filter_buffer"),
+	{ ngx_string("jpeg_filter_buffer"),
 	  NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
 	  ngx_conf_set_size_slot,
 	  NGX_HTTP_LOC_CONF_OFFSET,
-	  offsetof(ngx_http_modjpeg_loc_conf_t, buffer_size),
+	  offsetof(ngx_http_jpeg_filter_loc_conf_t, buffer_size),
 	  NULL },
 
 	ngx_null_command
 };
 
-static ngx_http_module_t  ngx_http_modjpeg_module_ctx = {
+static ngx_http_module_t  ngx_http_jpeg_filter_module_ctx = {
     NULL,                                  /* preconfiguration */
-    ngx_http_modjpeg_filter_init,          /* postconfiguration */
+    ngx_http_jpeg_filter_init,          /* postconfiguration */
 
     NULL,                                  /* create main configuration */
     NULL,                                  /* init main configuration */
@@ -83,26 +83,26 @@ static ngx_http_module_t  ngx_http_modjpeg_module_ctx = {
     NULL,                                  /* create server configuration */
     NULL,                                  /* merge server configuration */
 
-    ngx_http_modjpeg_create_conf,          /* create location configuration */
-    ngx_http_modjpeg_merge_conf            /* merge location configuration */
+    ngx_http_jpeg_filter_create_conf,          /* create location configuration */
+    ngx_http_jpeg_filter_merge_conf            /* merge location configuration */
 };
 
-static ngx_int_t ngx_http_modjpeg_header_filter(ngx_http_request_t *r) {
+static ngx_int_t ngx_http_jpeg_header_filter(ngx_http_request_t *r) {
 	off_t                          len;
-	ngx_http_modjpeg_ctx_t   *ctx;
-	ngx_http_modjpeg_conf_t  *conf;
+	ngx_http_jpeg_filter_ctx_t   *ctx;
+	ngx_http_jpeg_filter_conf_t  *conf;
 
 	if (r->headers_out.status == NGX_HTTP_NOT_MODIFIED) {
 		return ngx_http_next_header_filter(r);
 	}
 
-	ctx = ngx_http_get_module_ctx(r, ngx_http_modjpeg_module);
+	ctx = ngx_http_get_module_ctx(r, ngx_http_jpeg_filter_module);
 	if(ctx) {
-		ngx_http_set_ctx(r, NULL, ngx_http_modjpeg_module);
+		ngx_http_set_ctx(r, NULL, ngx_http_jpeg_filter_module);
 		return ngx_http_next_header_filter(r);
 	}
 
-	conf = ngx_http_get_module_loc_conf(r, ngx_http_modjpeg_module);
+	conf = ngx_http_get_module_loc_conf(r, ngx_http_jpeg_filter_module);
 
 	if(conf->enabled == 0) {
 		return ngx_http_next_header_filter(r);
@@ -116,7 +116,7 @@ static ngx_int_t ngx_http_modjpeg_header_filter(ngx_http_request_t *r) {
 			sizeof("multipart/x-mixed-replace") - 1
 		) == 0
 	) {
-		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "modjpeg: multipart/x-mixed-replace response");
+		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "jpeg_filter: multipart/x-mixed-replace response");
 
 		return NGX_ERROR;
 	}
@@ -126,12 +126,12 @@ static ngx_int_t ngx_http_modjpeg_header_filter(ngx_http_request_t *r) {
 		return NGX_ERROR;
 	}
 
-	ngx_http_set_ctx(r, ctx, ngx_http_modjpeg_module);
+	ngx_http_set_ctx(r, ctx, ngx_http_jpeg_filter_module);
 
 	len = r->headers_out.content_length_n;
 
 	if(len != -1 && len > (off_t)conf->buffer_size) {
-		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "modjpeg: too big response: %O", len);
+		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "jpeg_filter: too big response: %O", len);
 
 		return NGX_HTTP_UNSUPPORTED_MEDIA_TYPE;
 	}
@@ -152,27 +152,27 @@ static ngx_int_t ngx_http_modjpeg_header_filter(ngx_http_request_t *r) {
 	return NGX_OK;
 }
 
-static ngx_int_t ngx_http_modjpeg_body_filter(ngx_http_request_t *r, ngx_chain_t *in) {
+static ngx_int_t ngx_http_jpeg_body_filter(ngx_http_request_t *r, ngx_chain_t *in) {
 	ngx_int_t			rc;
 	ngx_str_t			*ct;
 	ngx_chain_t			out;
-	ngx_http_image_filter_ctx_t	*ctx;
-	ngx_http_image_filter_conf_t	*conf;
+	ngx_http_jpeg_filter_ctx_t	*ctx;
+	ngx_http_jpeg_filter_conf_t	*conf;
 
-	ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "modjpeg filter");
+	ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "jpeg_filter");
 
 	if(in == NULL) {
 		return ngx_http_next_body_filter(r, in);
 	}
 
-	ctx = ngx_http_get_module_ctx(r, ngx_http_modjpeg_module);
+	ctx = ngx_http_get_module_ctx(r, ngx_http_jpeg_filter_module);
 	if(ctx == NULL) {
 		return ngx_http_next_body_filter(r, in);
 	}
 
 	switch(ctx->phase) {
 	case NGX_HTTP_IMAGE_START:
-		if(ngx_http_modjpeg_test(r, in) == NGX_ERROR) {
+		if(ngx_http_jpeg_filter_test(r, in) == NGX_ERROR) {
 			return ngx_http_next_body_filter(r, in);
 		}
 
@@ -182,27 +182,27 @@ static ngx_int_t ngx_http_modjpeg_body_filter(ngx_http_request_t *r, ngx_chain_t
 		ctx->phase = NGX_HTTP_IMAGE_READ;
 
 	case NGX_HTTP_IMAGE_READ:
-		rc = ngx_http_modjpeg_read(r, in);
+		rc = ngx_http_jpeg_filter_read(r, in);
 
 		if (rc == NGX_AGAIN) {
 			return NGX_OK;
 		}
 
 		if (rc == NGX_ERROR) {
-			return ngx_http_filter_finalize_request(r, &ngx_http_modjpeg_module, NGX_HTTP_UNSUPPORTED_MEDIA_TYPE);
+			return ngx_http_filter_finalize_request(r, &ngx_http_jpeg_filter_module, NGX_HTTP_UNSUPPORTED_MEDIA_TYPE);
 		}
 
 	case NGX_HTTP_IMAGE_PROCESS:
-		out.buf = ngx_http_modjpeg_process(r);
+		out.buf = ngx_http_jpeg_filter_process(r);
 
 		if(out.buf == NULL) {
-			return ngx_http_filter_finalize_request(r, &ngx_http_modjpeg_module, NGX_HTTP_UNSUPPORTED_MEDIA_TYPE);
+			return ngx_http_filter_finalize_request(r, &ngx_http_jpeg_filter_module, NGX_HTTP_UNSUPPORTED_MEDIA_TYPE);
 		}
 
 		out.next = NULL;
 		ctx->phase = NGX_HTTP_IMAGE_PASS;
 
-		return ngx_http_modjpeg_send(r, ctx, &out);
+		return ngx_http_jpeg_filter_send(r, ctx, &out);
 
 	case NGX_HTTP_IMAGE_PASS:
 		rc = ngx_http_next_body_filter(r, in);
@@ -215,7 +215,7 @@ static ngx_int_t ngx_http_modjpeg_body_filter(ngx_http_request_t *r, ngx_chain_t
 	}
 }
 
-static ngx_int_t ngx_http_modjpeg_send(ngx_http_request_t *r, ngx_http_modjpeg_ctx_t *ctx, ngx_chain_t *in) {
+static ngx_int_t ngx_http_jpeg_filter_send(ngx_http_request_t *r, ngx_http_jpeg_filter_ctx_t *ctx, ngx_chain_t *in) {
 	ngx_int_t  rc;
 
 	rc = ngx_http_next_header_filter(r);
@@ -234,7 +234,7 @@ static ngx_int_t ngx_http_modjpeg_send(ngx_http_request_t *r, ngx_http_modjpeg_c
 	return rc;
 }
 
-static ngx_uint_t ngx_http_modjpeg_test(ngx_http_request_t *r, ngx_chain_t *in)
+static ngx_uint_t ngx_http_jpeg_filter_test(ngx_http_request_t *r, ngx_chain_t *in)
 {
 	u_char  *p;
 
@@ -244,7 +244,7 @@ static ngx_uint_t ngx_http_modjpeg_test(ngx_http_request_t *r, ngx_chain_t *in)
 		return NGX_ERROR;
 	}
 
-	ngx_log_debug2(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "modjpeg filter: \"%c%c\"", p[0], p[1]);
+	ngx_log_debug2(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "jpeg_filter: \"%c%c\"", p[0], p[1]);
 
 	if(p[0] == 0xff && p[1] == 0xd8) { /* JPEG */
 		return NGX_OK;
@@ -253,14 +253,14 @@ static ngx_uint_t ngx_http_modjpeg_test(ngx_http_request_t *r, ngx_chain_t *in)
 	return NGX_ERROR;
 }
 
-static ngx_int_t ngx_http_modjpeg_read(ngx_http_request_t *r, ngx_chain_t *in) {
+static ngx_int_t ngx_http_jpeg_filter_read(ngx_http_request_t *r, ngx_chain_t *in) {
 	u_char				*p;
 	size_t				size, rest;
 	ngx_buf_t			*b;
 	ngx_chain_t			*cl;
-	ngx_http_image_filter_ctx_t	*ctx;
+	ngx_http_jpeg_filter_ctx_t	*ctx;
 
-	ctx = ngx_http_get_module_ctx(r, ngx_http_modjpeg_module);
+	ctx = ngx_http_get_module_ctx(r, ngx_http_jpeg_filter_module);
 
 	if (ctx->image == NULL) {
 		ctx->image = ngx_palloc(r->pool, ctx->length);
@@ -282,7 +282,7 @@ static ngx_int_t ngx_http_modjpeg_read(ngx_http_request_t *r, ngx_chain_t *in) {
 		rest = ctx->image + ctx->length - p;
 
 		if(size > rest) {
-			ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "modjpeg filter: too big response");
+			ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "jpeg_filter: too big response");
 			return NGX_ERROR;
 		}
 
@@ -301,14 +301,14 @@ static ngx_int_t ngx_http_modjpeg_read(ngx_http_request_t *r, ngx_chain_t *in) {
 	return NGX_AGAIN;
 }
 
-static ngx_buf_t *ngx_http_image_process(ngx_http_request_t *r) {
+static ngx_buf_t *ngx_http_jpeg_filter_process(ngx_http_request_t *r) {
 	return NULL;
 }
 
-static void *ngx_http_modjpeg_create_loc_conf(ngx_conf_t *cf) {
-	ngx_http_modjpeg_loc_conf_t  *conf;
+static void *ngx_http_jpeg_filter_create_loc_conf(ngx_conf_t *cf) {
+	ngx_http_jpeg_filter_loc_conf_t  *conf;
 
-	conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_modjpeg_loc_conf_t));
+	conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_jpeg_filter_loc_conf_t));
 	if(conf == NULL) {
 		return NGX_CONF_ERROR;
 	}
@@ -323,9 +323,9 @@ static void *ngx_http_modjpeg_create_loc_conf(ngx_conf_t *cf) {
 	return conf;
 }
 
-static char *ngx_http_modjpeg_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child) {
-	ngx_http_modjpeg_loc_conf_t *prev = parent;
-	ngx_http_modjpeg_loc_conf_t *conf = child;
+static char *ngx_http_jpeg_filter_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child) {
+	ngx_http_jpeg_filter_loc_conf_t *prev = parent;
+	ngx_http_jpeg_filter_loc_conf_t *conf = child;
 
 	ngx_conf_merge_value(conf->enable, prev->enable, 0);
 	ngx_conf_merge_value(conf->optimize, prev->optimize, 0);
@@ -347,10 +347,10 @@ static char *ngx_http_modjpeg_merge_loc_conf(ngx_conf_t *cf, void *parent, void 
 	return NGX_CONF_OK;
 }
 
-ngx_module_t  ngx_http_modjpeg_module = {
+ngx_module_t  ngx_http_jpeg_filter_module = {
 	NGX_MODULE_V1,
-	&ngx_http_modjpeg_module_ctx, /* module context */
-	ngx_http_modjpeg_commands,   /* module directives */
+	&ngx_http_jpeg_filter_module_ctx, /* module context */
+	ngx_http_jpeg_filter_commands,   /* module directives */
 	NGX_HTTP_MODULE,               /* module type */
 	NULL,                          /* init master */
 	NULL,                          /* init module */
@@ -362,12 +362,12 @@ ngx_module_t  ngx_http_modjpeg_module = {
 	NGX_MODULE_V1_PADDING
 };
 
-static ngx_int_t ngx_http_modjpeg_filter_init(ngx_conf_t *cf) {
+static ngx_int_t ngx_http_jpeg_filter_init(ngx_conf_t *cf) {
 	ngx_http_next_header_filter = ngx_http_top_header_filter;
-	ngx_http_top_header_filter = ngx_http_modjpeg_header_filter;
+	ngx_http_top_header_filter = ngx_http_jpeg_header_filter;
 
 	ngx_http_next_body_filter = ngx_http_top_body_filter;
-	ngx_http_top_body_filter = ngx_http_modjpeg_body_filter;
+	ngx_http_top_body_filter = ngx_http_jpeg_body_filter;
 
 	return NGX_OK;
 }
